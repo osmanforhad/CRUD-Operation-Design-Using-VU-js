dcfc13a56c16374ea03f18a3b345103bde5d25c2
osmanforhad <osmanforhad93@gmail.com>
2018-11-22 01:44:44 +0600

    Design A CRUD Operation Using VUE js Framework

---
+++ CRUD-Operation-Design-Using-VU-js/VUE_PHP_CRUD/app.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var app = new Vue({
+    el: "#root",
+    data: {
+        showingAddModel: false,
+        showingEditModel: false,
+        showingDeleteModel: false
+    },
+    mounted: function (){
+        console.log("mounted");
+    },
+    methods: {
+        
+    }
+});
+
+
